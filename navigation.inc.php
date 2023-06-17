a573ecb2073d5d418f945a3420b3e9149eca5491
Sebastian Gerhaupt <gerhaupt.sebastian@gmail.com>
2023-06-17 20:12:12 +0200

    Add files via upload
    Added small files that will be included by the files requested by users.

---
+++ navigation.inc.php
@@ -0,0 +1,23 @@
+<nav class="column-3">
+	<ul class="navigation sticky">
+		<div class="icon-arrow-right-short" id="navigation-icon">
+			<?php include("images/icon_arrow_right_short.svg"); ?>
+		</div>
+		<li>
+			<a class="root-link navigation-link" href=".">
+				<span class="no-reload-label">no_reload</span>
+			</a>
+		</li>
+		<li>
+			<a class="reload-link navigation-link" href="download.php">Download</a>
+		</li>
+		<li>
+			<a class="reload-link navigation-link" href="verwendung.php">Verwendung</a>
+		</li>
+		<!--ToDo:
+		<li>
+			<a class="reload-link navigation-link" href="der_code.php">Der Code</a>
+		</li>
+		-->
+	</ul>
+</nav>
